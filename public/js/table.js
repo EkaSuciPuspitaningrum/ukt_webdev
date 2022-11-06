11ba8f254d41b72d9885eeecdf6c51c6d20a4d34
Eka Suci Puspitaningrum <ekaasuci241@gmail.com>
2022-11-06 22:53:19 +0700

    first commit

---
+++ public/js/table.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#table').DataTable({
+        scrollX: true,
+        autoWidth: false,
+        paging: false,
+        info: false,
+        searching:false,
+        responsive:true,
+        ordering: false,
+    });
+});
+
+$(document).ready(function () {
+    $('#table1').DataTable({
+        scrollX: true,
+        autoWidth: false,
+        paging: false,
+        info: false,
+        searching:false,
+        responsive:true,
+        ordering: false,
+    });
+});
